f65fb510730aa2524f6f061e754e8afd651d1327
JoseCarlosPa <josecarlospas1@gmail.com>
2021-10-24 16:24:20 -0500

    feat: Se completa la logica para el Drag and drop dependiendo del source Id que nos regrese la API

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import InputFeild from './components/InputFeild';
 import {ToDo} from "./models/model";
 import TodoList from "./components/TodoList";
-import {DragDropContext} from 'react-beautiful-dnd';
+import {DragDropContext, DropResult} from 'react-beautiful-dnd';
 
 const App: React.FC = () => {
 
@@ -20,10 +20,56 @@ const App: React.FC = () => {
             setToDo("");
         }
     }
-    console.log(toDo);
+
+
+    const onDragEnd = (result: DropResult) => {
+
+
+        const {source, destination} = result;
+        if (!destination) return;
+        if (destination.droppableId === source.droppableId && destination.index === source.index) return;
+        let add,
+            active = toDos,
+            progress = doing,
+            complete = completedToDos;
+
+        // Checamos a que parte del Droppable id corresponde y se lo quitamos según nos regrese el API
+
+        if (source.droppableId === 'ToDoList') {
+            add = active[source.index];
+            active.splice(source.index, 1);
+        } else if(source.droppableId === 'Doing') {
+            add = progress[source.index];
+            progress.splice(source.index, 1);
+
+        }else{
+            add = complete[source.index];
+            complete.splice(source.index, 1);
+
+        }
+
+
+
+        // Ahora Agregamos lo que se quito al source destino
+        if (destination.droppableId === 'ToDoList') {
+            active.splice(destination.index,0,add)
+
+        } else if(destination.droppableId === 'Doing') {
+            progress.splice(destination.index,0,add)
+            console.log('paso por Doing');
+        }else{
+            complete.splice(destination.index,0,add)
+        }
+
+        // Agregarlo a nuestro estado
+        setcompletedToDos(complete);
+        setDoing(progress);
+        setToDos(active);
+
+    }
 
     return (
-        <DragDropContext onDragEnd={()=>{}}>
+        <DragDropContext onDragEnd={onDragEnd}>
             <div className="App">
                 <span className="heading">LeadSales</span>
                 <InputFeild
